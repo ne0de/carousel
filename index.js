d3fb54c69ac0f44795a8a25e1ad597052680fb5a
nenedodo <nani.andres@hotmail.com.ar>
2022-02-25 15:16:06 -0300

    added thumbnail scripts

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ var carouselIndex = 0;
 
 OnCarouselChange(carouselIndex)
 
-function OnCarouselChange(value) {
-    UpdateCarousel(value);
+function OnCarouselChange(value, throughButton = true) {
+    UpdateCarousel(value, throughButton);
 }
 
 function GetCarouselItems() {
-    return document.getElementsByClassName('carousel-item');
+    return document.getElementsByClassName('carousel-img');
+}
+
+function GetThumbnailsItems() {
+    return document.getElementsByClassName('carousel-thumbnail')
+}
+
+function OnThumbnailUpdate(index, selected = false) {
+    var thumbnails = GetThumbnailsItems()
+    var element = thumbnails[index]
+    console.log(index)
+    element.className = 'carousel-thumbnail '
+    element.className += selected ? 'selected' : 'unselected'
 }
 
 function OnCarouselItemUpdate(index, hide = true) {
@@ -17,20 +29,25 @@ function OnCarouselItemUpdate(index, hide = true) {
 }
 
 
-function UpdateCarousel(number) {
+function UpdateCarousel(number, throughButton) {
     var carousels = GetCarouselItems()
+    var thumbnails = GetThumbnailsItems()
     var maxCarousels = carousels.length - 1
 
-    carouselIndex += number
-
-    if (carouselIndex > maxCarousels) 
-        carouselIndex = 0;
+    if(throughButton)
+        carouselIndex += number
+    else
+        carouselIndex = number
     
-    if (carouselIndex < 0)
-        carouselIndex = maxCarousels
+    if (carouselIndex > maxCarousels) carouselIndex = 0;
+    
+    if (carouselIndex < 0) carouselIndex = maxCarousels
 
-    for (let index = 0; index < carousels.length; index++)
+    for (let index = 0; index < carousels.length; index++) {
         OnCarouselItemUpdate(index)
+        OnThumbnailUpdate(index)
+    }   
 
+    OnThumbnailUpdate(carouselIndex, true)  
     OnCarouselItemUpdate(carouselIndex, false)
 }
